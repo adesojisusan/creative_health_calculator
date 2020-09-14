f31706e4ec5ba52f41237c343dfe5c75b5f1f66e
Susan Adesoji <adesojisusan.adeola@gmail.com>
2020-09-14 16:33:38 +0100

    Custom

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const BottomHeight = 80.0;
+const ActiveCardColor = Colors.blue;
 
 class InputPage extends StatefulWidget {
   @override
@@ -19,7 +20,7 @@ class _InputPageState extends State<InputPage> {
           Expanded(child: Row(
             children: <Widget>[
               Expanded(
-                child: ReuseableCard(colour:Colors.black),
+                child: ReuseableCard(colour:ActiveCardColor,),
               ),
               Expanded(
                 child: ReuseableCard(colour:Colors.pink),
